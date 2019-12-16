daac4b4d30d136a4908210cb70ddf0cd8efafb15
Tomasz Morek <t.morek@gmail.com>
2019-12-16 22:18:00 +0100

    #6 Test 1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,15 @@ val kotlinVersion = plugins.getPlugin(KotlinPluginWrapper::class.java).kotlinPlu
 
 plugins {
     kotlin("jvm") version "1.3.60"
+    jacoco
+    id("org.sonarqube") version("2.7.1")
 }
 
 allprojects {
     group = "com.tm.rankme"
     version = "0.2-SNAPSHOT"
+    
+    apply(plugin = "jacoco")
 
     repositories {
         mavenCentral()
@@ -18,6 +22,16 @@ allprojects {
 
 subprojects {
     apply(plugin = "kotlin")
+    apply(plugin = "jacoco")
+
+    sonarqube {
+        properties {
+            property("sonar.sources", "src/main")
+            property("sonar.tests", "src/test")
+//            property("sonar.jacoco.reportPath", "build/jacoco/test.exec")
+//            property("sonar.test.exclusions", "src/test")
+        }
+    }
 
     dependencies {
         implementation(kotlin("stdlib-jdk8"))
@@ -31,5 +45,11 @@ subprojects {
 
     tasks.test {
         useJUnitPlatform()
+    }
+}
+
+tasks.register("info") {
+    doLast {
+        println(project.base)
     }
 }
