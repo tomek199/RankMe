b71580a605b275b196c69ebf54c6dd9ac08fd6ed
Tomasz Morek <t.morek@gmail.com>
2020-03-14 20:49:18 +0100

    #29 Update Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val kotlinVersion = plugins.getPlugin(KotlinPluginWrapper::class.java).kotlinPluginVersion
 
 plugins {
-    kotlin("jvm") version "1.3.60"
+    kotlin("jvm") version "1.3.70"
     jacoco
     id("org.sonarqube") version("2.7.1")
 }
