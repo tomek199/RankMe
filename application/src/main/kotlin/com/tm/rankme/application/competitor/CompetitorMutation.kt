ea8159ae3dcea4faf68171a48e4421cd0d7b2974
Tomasz Morek <t.morek@gmail.com>
2020-03-14 17:49:54 +0100

    #24 Throw exception when league does not exist

--- application/src/main/kotlin/com/tm/rankme/application/competitor/CompetitorMutation.kt
+++ application/src/main/kotlin/com/tm/rankme/application/competitor/CompetitorMutation.kt
@@ -4,16 +4,19 @@ import com.coxautodev.graphql.tools.GraphQLMutationResolver
 import com.tm.rankme.application.common.Mapper
 import com.tm.rankme.domain.competitor.Competitor
 import com.tm.rankme.domain.competitor.CompetitorRepository
+import com.tm.rankme.domain.league.LeagueRepository
 import org.springframework.stereotype.Service
 
 @Service
 class CompetitorMutation(
-        private val repository: CompetitorRepository,
+        private val competitorRepository: CompetitorRepository,
+        private val leagueRepository: LeagueRepository,
         private val mapper: Mapper<Competitor, CompetitorModel>
 ) : GraphQLMutationResolver {
     fun addCompetitor(leagueId: String, username: String): CompetitorModel {
+        leagueRepository.findById(leagueId)?:  throw IllegalStateException("League does not exist!")
         val domain = Competitor(leagueId, username)
-        val competitor = repository.save(domain)
+        val competitor = competitorRepository.save(domain)
         return mapper.toModel(competitor)
     }
 }
--- application/src/test/kotlin/com/tm/rankme/application/competitor/CompetitorMutationTest.kt
+++ application/src/test/kotlin/com/tm/rankme/application/competitor/CompetitorMutationTest.kt
@@ -4,21 +4,25 @@ import com.tm.rankme.application.common.Mapper
 import com.tm.rankme.domain.competitor.Competitor
 import com.tm.rankme.domain.competitor.CompetitorRepository
 import com.tm.rankme.domain.competitor.Statistics
+import com.tm.rankme.domain.league.League
+import com.tm.rankme.domain.league.LeagueRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
-
 @ExtendWith(MockitoExtension::class)
 internal class CompetitorMutationTest {
-    private val repository = Mockito.mock(CompetitorRepository::class.java)
+    private val competitorRepository = Mockito.mock(CompetitorRepository::class.java)
+    private val leagueRepository = Mockito.mock(LeagueRepository::class.java)
     private val mapper: Mapper<Competitor, CompetitorModel> = CompetitorMapper()
-    private val mutation = CompetitorMutation(repository, mapper)
+    private val mutation = CompetitorMutation(competitorRepository, leagueRepository, mapper)
 
     private val leagueId = "l-111"
     private val username = "Optimus Prime"
@@ -26,13 +30,14 @@ internal class CompetitorMutationTest {
 
     @BeforeEach
     internal fun setUp() {
-        given(repository.save(any(Competitor::class.java)))
+        given(competitorRepository.save(any(Competitor::class.java)))
                 .willReturn(Competitor(leagueId, "c-111", username, Statistics()))
     }
 
     @Test
     internal fun `Should add competitor with default params`() {
         // given
+        given(leagueRepository.findById(leagueId)).willReturn(League(leagueId, "Transformers"))
         val expectedStatistics = Statistics()
         // when
         val competitor = mutation.addCompetitor(leagueId, username)
@@ -48,5 +53,15 @@ internal class CompetitorMutationTest {
         assertEquals(expectedStatistics.draw, competitor.statistics.draw)
     }
 
-    private fun <T> any(type: Class<T>): T = Mockito.any<T>(type)
+    @Test
+    internal fun `Should throw exception when league does not exist`() {
+        // given
+        // when
+        given(leagueRepository.findById(leagueId)).willReturn(null)
+        // then
+        assertFailsWith<IllegalStateException> { mutation.addCompetitor(leagueId, username) }
+    }
+
+    private fun <T> any(type: Class<T>): T = Mockito.any(type)
 }
+
