82cb24c07c0a4a59545fed7d254946bab413c89c
Tomasz Morek <t.morek@gmail.com>
2020-08-11 17:53:12 +0200

    #48 Replace deprecated constructor for date time scalars (#52)
    * #48 Version bump up
    * #48 Create bean for LocalDate scalar
    * #48 Create bean for LocalDateTime scalar

--- application/src/main/kotlin/com/tm/rankme/application/Application.kt
+++ application/src/main/kotlin/com/tm/rankme/application/Application.kt
@@ -1,13 +1,34 @@
 package com.tm.rankme.application
 
+import com.tm.rankme.application.common.LocalDateCoercing
+import com.tm.rankme.application.common.LocalDateTimeCoercing
+import graphql.schema.GraphQLScalarType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 
 
 @SpringBootApplication
 @ComponentScan(value = ["com.tm.rankme.application", "com.tm.rankme.infrastructure"])
-class Application
+class Application {
+
+    @Bean
+    fun localDateScalar(): GraphQLScalarType {
+        return GraphQLScalarType.newScalar()
+            .name("LocalDate")
+            .coercing(LocalDateCoercing())
+            .build()
+    }
+
+    @Bean
+    fun localDateTimeScalar(): GraphQLScalarType {
+        return GraphQLScalarType.newScalar()
+            .name("LocalDateTime")
+            .coercing(LocalDateTimeCoercing())
+            .build()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
