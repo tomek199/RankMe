03906286f3418438587754b694301b54159bf439
Tomasz Morek <t.morek@gmail.com>
2020-09-21 20:44:21 +0200

    #50 Logger for GraphQL actions

--- application/src/main/kotlin/com/tm/rankme/application/league/LeagueQuery.kt
+++ application/src/main/kotlin/com/tm/rankme/application/league/LeagueQuery.kt
@@ -1,11 +1,15 @@
 package com.tm.rankme.application.league
 
+import com.tm.rankme.application.common.logger
 import graphql.kickstart.tools.GraphQLQueryResolver
 import org.springframework.stereotype.Service
 
 @Service
 class LeagueQuery(private val leagueService: LeagueService) : GraphQLQueryResolver {
+    private val log = logger<LeagueQuery>()
+
     fun league(id: String): LeagueModel? {
+        log.info("Get league: id=$id")
         return leagueService.get(id)
     }
 }
