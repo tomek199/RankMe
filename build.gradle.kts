21203f20ffb4bfe458ce8eec3d20ccbd1bb979e7
Tomasz Morek <t.morek@gmail.com>
2020-11-19 23:10:38 +0100

    #70 Add EventStore docker, configuration and connector

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,9 @@ allprojects {
 
     repositories {
         mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
     }
 }
 
--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
     implementation(project(":domain"))
     implementation("org.springframework:spring-context:5.3.0")
+    implementation("com.github.EventStore:EventStoreDB-Client-Java:trunk-SNAPSHOT")
+    testImplementation("org.mockito:mockito-inline:3.3.3")
 }
