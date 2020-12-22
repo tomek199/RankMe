5a6c31f93f4da7612247e06923d5617f94b7e900
Tomasz Morek <t.morek@gmail.com>
2020-12-22 12:24:19 +0100

    #75 Combine duplicated tests

--- application/src/test/kotlin/com/tm/rankme/domain/league/LeagueRepositoryTest.kt
+++ application/src/test/kotlin/com/tm/rankme/domain/league/LeagueRepositoryTest.kt
@@ -7,6 +7,8 @@ import io.mockk.mockk
 import io.mockk.verify
 import io.mockk.verifySequence
 import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class LeagueRepositoryTest {
@@ -14,6 +16,45 @@ internal class LeagueRepositoryTest {
     private val eventEmitter = mockk<EventEmitter>(relaxed = true)
     private val repository = object : LeagueRepository(eventStorage, eventEmitter) { }
 
+    @Test
+    internal fun `Should get league by id`() {
+        // given
+        val aggregateId = UUID.randomUUID()
+        val created = LeagueCreated("Star Wars", aggregateId = aggregateId)
+        val renamed = LeagueRenamed(aggregateId, 1, "Transformers")
+        val settingsChanged = LeagueSettingsChanged(aggregateId, 2, true, 7)
+        every { eventStorage.events(aggregateId.toString()) } returns listOf(created, renamed, settingsChanged)
+        // when
+        val league = repository.byId(aggregateId)
+        // then
+        assertEquals(aggregateId, league.id)
+        assertEquals(2, league.version)
+        assertTrue(league.pendingEvents.isEmpty())
+        assertEquals(renamed.name, league.name)
+        assertEquals(settingsChanged.allowDraws, league.settings.allowDraws)
+        assertEquals(settingsChanged.maxScore, league.settings.maxScore)
+    }
+
+    @Test
+    internal fun `Should store league`() {
+        // given
+        val league = League.create("Star Wars")
+        league.settings(true, 7)
+        league.rename("Transformers")
+        // when
+        repository.store(league)
+        // then
+        verifySequence {
+            eventStorage.save(ofType(LeagueCreated::class))
+            eventEmitter.emit(ofType(LeagueCreated::class))
+            eventStorage.save(ofType(LeagueSettingsChanged::class))
+            eventEmitter.emit(ofType(LeagueSettingsChanged::class))
+            eventStorage.save(ofType(LeagueRenamed::class))
+            eventEmitter.emit(ofType(LeagueRenamed::class))
+        }
+    }
+
+    // todo remove
     @Test
     internal fun `Should get league by id using event storage`() {
         // given
@@ -26,6 +67,7 @@ internal class LeagueRepositoryTest {
         verify(exactly = 1) { eventStorage.events(aggregateId.toString()) }
     }
 
+    // todo remove
     @Test
     internal fun `Should store event using event storage and event emitter`() {
         // given
--- infrastructure/src/test/kotlin/com/tm/rankme/storage/write/league/EventSourceLeagueRepositoryTest.kt
+++ infrastructure/src/test/kotlin/com/tm/rankme/storage/write/league/EventSourceLeagueRepositoryTest.kt
@@ -1,58 +1,19 @@
 package com.tm.rankme.storage.write.league
 
-import com.tm.rankme.domain.league.League
-import com.tm.rankme.domain.league.LeagueCreated
-import com.tm.rankme.domain.league.LeagueRenamed
-import com.tm.rankme.domain.league.LeagueSettingsChanged
 import com.tm.rankme.domain.base.EventEmitter
-import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verifySequence
-import java.util.*
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import kotlin.test.assertNotNull
 import org.junit.jupiter.api.Test
 
 internal class EventSourceLeagueRepositoryTest {
-    private val eventStorage = mockk<LeagueEventStorage>(relaxed = true)
-    private val eventEmitter = mockk<EventEmitter>(relaxed = true)
-    private val repository = EventSourceLeagueRepository(eventStorage, eventEmitter)
-
     @Test
-    internal fun `Should get league by id`() {
+    internal fun `Should create repository`() {
         // given
-        val aggregateId = UUID.randomUUID()
-        val created = LeagueCreated("Star Wars", aggregateId = aggregateId)
-        val renamed = LeagueRenamed(aggregateId, 1, "Transformers")
-        val settingsChanged = LeagueSettingsChanged(aggregateId, 2, true, 7)
-        every { eventStorage.events(aggregateId.toString()) } returns listOf(created, renamed, settingsChanged)
+        val eventEmitter = mockk<EventEmitter>()
+        val eventStorage = mockk<LeagueEventStorage>()
         // when
-        val league = repository.byId(aggregateId)
+        val repository = EventSourceLeagueRepository(eventStorage, eventEmitter)
         // then
-        assertEquals(aggregateId, league.id)
-        assertEquals(2, league.version)
-        assertTrue(league.pendingEvents.isEmpty())
-        assertEquals(renamed.name, league.name)
-        assertEquals(settingsChanged.allowDraws, league.settings.allowDraws)
-        assertEquals(settingsChanged.maxScore, league.settings.maxScore)
-    }
-
-    @Test
-    internal fun `Should store league`() {
-        // given
-        val league = League.create("Star Wars")
-        league.settings(true, 7)
-        league.rename("Transformers")
-        // when
-        repository.store(league)
-        // then
-        verifySequence {
-            eventStorage.save(ofType(LeagueCreated::class))
-            eventEmitter.emit(ofType(LeagueCreated::class))
-            eventStorage.save(ofType(LeagueSettingsChanged::class))
-            eventEmitter.emit(ofType(LeagueSettingsChanged::class))
-            eventStorage.save(ofType(LeagueRenamed::class))
-            eventEmitter.emit(ofType(LeagueRenamed::class))
-        }
+        assertNotNull(repository)
     }
 }
