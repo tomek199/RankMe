1ba88580a1dba0cdbf298c26179f904a78d9baf2
Tomasz Morek <t.morek@gmail.com>
2020-12-23 20:38:01 +0100

    #86 Add player repository

---
+++ infrastructure/src/main/kotlin/com/tm/rankme/storage/read/player/MongoPlayerRepository.kt
@@ -0,0 +1,18 @@
+package com.tm.rankme.storage.read.player
+
+import com.tm.rankme.model.player.Player
+import com.tm.rankme.model.player.PlayerRepository
+import java.util.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
+
+@Repository
+class MongoPlayerRepository @Autowired constructor(
+    private val playerAccessor: MongoPlayerAccessor
+) : PlayerRepository {
+
+    override fun byId(id: UUID): Player? = playerAccessor.findByIdOrNull(id)?.let {
+        Player(it.id, it.name, it.deviation, it.rating)
+    }
+}
