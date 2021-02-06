b2fea7b0f2614ca483aa5b3dda72bc6543b3cedd
Tomasz Morek <t.morek@gmail.com>
2021-02-06 23:52:03 +0100

    #100 Add RabbitMQ configuration for query handlers

--- query-service/src/main/kotlin/com/tm/rankme/cqrs/query/league/GetLeagueHandler.kt
+++ query-service/src/main/kotlin/com/tm/rankme/cqrs/query/league/GetLeagueHandler.kt
@@ -3,6 +3,10 @@ package com.tm.rankme.cqrs.query.league
 import com.tm.rankme.cqrs.query.QueryHandler
 import com.tm.rankme.model.league.League
 import com.tm.rankme.model.league.LeagueRepository
+import org.springframework.amqp.rabbit.annotation.Exchange
+import org.springframework.amqp.rabbit.annotation.Queue
+import org.springframework.amqp.rabbit.annotation.QueueBinding
+import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,5 +14,12 @@ class GetLeagueHandler(
     private val repository: LeagueRepository
 ) : QueryHandler<GetLeagueQuery, League?> {
 
+    @RabbitListener(bindings = [
+        QueueBinding(
+            value = Queue(name = "get-league-query-queue"),
+            exchange = Exchange(name = "rankme.api", type = "direct"),
+            key = ["GetLeagueQuery"]
+        )
+    ])
     override fun handle(query: GetLeagueQuery): League? = repository.byId(query.id)
 }
