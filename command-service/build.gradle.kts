e7ad608be7ec0a10b85e4e1aaaeacfd7b74ebba2
Tomasz Morek <t.morek@gmail.com>
2021-02-07 20:57:52 +0100

    #102 Add configuration for PostgreSQL

--- command-service/build.gradle.kts
+++ command-service/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.3")
 
     implementation("com.github.EventStore:EventStoreDB-Client-Java:trunk-SNAPSHOT")
