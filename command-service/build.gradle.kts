2b9459e16d31673b5e88da3a416d059168416a41
Tomasz Morek <t.morek@gmail.com>
2021-02-18 18:28:47 +0100

    #108 Move gradle configs to particular modules

--- command-service/build.gradle.kts
+++ command-service/build.gradle.kts
@@ -1,8 +1,21 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    kotlin("jvm") version "1.4.30"
+    jacoco
+    id("org.sonarqube") version("2.7.1")
     id("org.springframework.boot") version "2.3.5.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("plugin.spring") version "1.4.30"
     kotlin("plugin.jpa") version "1.4.30"
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
 }
 
 dependencies {
@@ -14,10 +27,31 @@ dependencies {
 
     implementation("com.github.EventStore:EventStoreDB-Client-Java:trunk-SNAPSHOT")
 
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:${plugins.getPlugin(KotlinPluginWrapper::class.java).kotlinPluginVersion}")
+    testImplementation("io.mockk:mockk:1.10.5")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
         exclude(group = "org.mockito", module = "mockito-core")
         exclude(group = "org.mockito", module = "mockito-junit-jupiter")
+    }
+}
+
+sonarqube { }
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = false
     }
 }
 
