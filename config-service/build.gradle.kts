b09f81c92b2c75a66a37bbe4455e817d37ad4651
Tomasz Morek <t.morek@gmail.com>
2021-02-27 21:36:15 +0100

    #113 Fix build (#114)

--- config-service/build.gradle.kts
+++ config-service/build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    kotlin("jvm")
     id("org.springframework.boot") version "2.4.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.30"
     kotlin("plugin.spring") version "1.4.30"
 }
 
