a79114ef07f87cab584782a939e9a9c5006e5f40
Tomasz Morek <t.morek@gmail.com>
2021-03-07 18:42:55 +0100

    #115 Register query-service in discovery-service

--- query-service/src/main/kotlin/com/tm/rankme/QueryService.kt
+++ query-service/src/main/kotlin/com/tm/rankme/QueryService.kt
@@ -2,8 +2,10 @@ package com.tm.rankme
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class QueryService
 
 fun main(args: Array<String>) {
