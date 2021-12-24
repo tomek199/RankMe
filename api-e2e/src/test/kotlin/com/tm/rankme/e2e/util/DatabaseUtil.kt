25880e6824b0f38859cbebb4c470a1515e9d9242
Tomasz Morek <t.morek@gmail.com>
2021-12-24 09:22:27 +0100

    #139 Improve DatabaseUtil

--- api-e2e/src/test/kotlin/com/tm/rankme/e2e/util/DatabaseUtil.kt
+++ api-e2e/src/test/kotlin/com/tm/rankme/e2e/util/DatabaseUtil.kt
@@ -1,14 +1,14 @@
 package com.tm.rankme.e2e.util
 
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Component
 
 @Component
 class DatabaseUtil(private val mongoTemplate: MongoTemplate) {
-    fun cleanup() {
-        mongoTemplate.remove(Query(), "league")
-        mongoTemplate.remove(Query(), "player")
-        mongoTemplate.remove(Query(), "game")
+    private val collections = listOf("league", "player", "game")
+
+    fun cleanup() = collections.forEach { collection ->
+        mongoTemplate.dropCollection(collection)
+        mongoTemplate.createCollection(collection)
     }
 }
