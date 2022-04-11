673ba3efa85ed6d591afa0a8a61fbb676f5ac71e
Tomasz Morek <t.morek@gmail.com>
2022-04-11 16:58:09 +0200

    #165 Add 'before' games query parameter

--- query-service/src/main/kotlin/com/tm/rankme/model/game/GameRepository.kt
+++ query-service/src/main/kotlin/com/tm/rankme/model/game/GameRepository.kt
@@ -5,9 +5,15 @@ import com.tm.rankme.model.Page
 interface GameRepository {
     fun byId(id: String): Game?
     fun store(game: Game)
-    fun byLeagueId(leagueId: String, first: Int, after: String? = null): Page<Game>
-    fun completedByLeagueId(leagueId: String, first: Int, after: String? = null): Page<Game>
-    fun scheduledByLeagueId(leagueId: String, first: Int, after: String? = null): Page<Game>
+    fun byLeagueId(leagueId: String, first: Int): Page<Game>
+    fun byLeagueIdAfter(leagueId: String, first: Int, after: String): Page<Game>
+    fun byLeagueIdBefore(leagueId: String, first: Int, before: String): Page<Game>
+    fun completedByLeagueId(leagueId: String, first: Int): Page<Game>
+    fun completedByLeagueIdAfter(leagueId: String, first: Int, after: String): Page<Game>
+    fun completedByLeagueIdBefore(leagueId: String, first: Int, before: String): Page<Game>
+    fun scheduledByLeagueId(leagueId: String, first: Int): Page<Game>
+    fun scheduledByLeagueIdAfter(leagueId: String, first: Int, after: String): Page<Game>
+    fun scheduledByLeagueIdBefore(leagueId: String, first: Int, before: String): Page<Game>
     fun byPlayerId(playerId: String, first: Int, after: String? = null): Page<Game>
     fun completedByPlayerId(playerId: String, first: Int, after: String? = null): Page<Game>
     fun scheduledByPlayerId(playerId: String, first: Int, after: String? = null): Page<Game>
