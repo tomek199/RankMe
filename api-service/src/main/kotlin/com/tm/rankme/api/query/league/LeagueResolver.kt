8678b6ae3f2d2506ab4e47a992b113e6e52ff51c
Tomasz Morek <t.morek@gmail.com>
2022-04-11 22:43:34 +0200

    #166 Add 'before' query parameter for league's games connection

--- api-service/src/main/kotlin/com/tm/rankme/api/query/league/LeagueResolver.kt
+++ api-service/src/main/kotlin/com/tm/rankme/api/query/league/LeagueResolver.kt
@@ -17,12 +17,15 @@ class LeagueResolver(
 
     fun players(league: League): List<Player> = playerQueryHandler.handle(GetPlayersQuery(league.id))
 
-    fun games(league: League, first: Int, after: String?, env: DataFetchingEnvironment): Connection<Game> =
-        gameQueryHandler.handle(GetGamesForLeagueQuery(league.id, first, after))
+    fun games(
+        league: League, first: Int, after: String?, before: String?, env: DataFetchingEnvironment
+    ): Connection<Game> = gameQueryHandler.handle(GetGamesForLeagueQuery(league.id, first, after, before))
 
-    fun completedGames(league: League, first: Int, after: String?, env: DataFetchingEnvironment): Connection<CompletedGame> =
-        gameQueryHandler.handle(GetCompletedGamesForLeagueQuery(league.id, first, after))
+    fun completedGames(
+        league: League, first: Int, after: String?, before: String?, env: DataFetchingEnvironment
+    ): Connection<CompletedGame> = gameQueryHandler.handle(GetCompletedGamesForLeagueQuery(league.id, first, after, before))
 
-    fun scheduledGames(league: League, first: Int, after: String?, env: DataFetchingEnvironment): Connection<ScheduledGame> =
-        gameQueryHandler.handle(GetScheduledGamesForLeagueQuery(league.id, first, after))
+    fun scheduledGames(
+        league: League, first: Int, after: String?, before: String?, env: DataFetchingEnvironment
+    ): Connection<ScheduledGame> = gameQueryHandler.handle(GetScheduledGamesForLeagueQuery(league.id, first, after, before))
 }
