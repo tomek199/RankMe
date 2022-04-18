5085db4da091364487c8903d12cb283d9441e36a
Tomasz Morek <t.morek@gmail.com>
2022-04-18 14:09:33 +0200

    #170 Update npm dependencies

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -31,7 +31,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatOptionModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { Apollo } from 'apollo-angular';
 
 @NgModule({
     declarations: [
