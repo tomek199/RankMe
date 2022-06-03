2fdefd422d3db6b486f6c065afa246891340eb74
Tomasz Morek <t.morek@gmail.com>
2022-06-03 21:24:23 +0200

    #182 Add empty player-list component

--- webapp/src/app/leagues/league.component.ts
+++ webapp/src/app/leagues/league.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateLeagueComponent } from './create-league/create-league.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-league',
   templateUrl: './league.component.html',
   styleUrls: ['./league.component.scss']
 })
-export class LeagueComponent {
+export class LeagueComponent implements OnInit {
+  leagueId: string;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.leagueId = params['league_id'];
+    });
+  }
 
   createLeague() {
     this.dialog.open(CreateLeagueComponent);
