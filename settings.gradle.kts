ed5db6e427e36f938da18c6c2afdc04ac06a84a0
Tomasz Morek <t.morek@gmail.com>
2022-10-27 18:32:07 +0200

    #192 Update subscription-service config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,9 @@ rootProject.name = "RankMe"
 
 include("config-service")
 include("discovery-service")
+include("gateway-service")
 include("api-service")
-include("subscription-service")
 include("command-service")
 include("query-service")
-include("gateway-service")
+include("subscription-service")
 include("api-e2e")
